e6454e3f94d9ffb03c04722a1ae3a52aa50d6622
Shinji-ux <shinji.iwasaki1218@gmail.com>
2024-08-16 21:01:26 +0900

    結合

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+Rails.root.glob('spec/support/**/*.rb').sort.each { |f| require f }
 
 I18n.locale = "en"
 
@@ -16,6 +17,7 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = Rails.root.join('spec/fixtures')
+  config.include SignInSupport
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
